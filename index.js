9278ba902cf023f52fbc450cde0a1242be12ab4d
rashmi123456789 <32570239+rashmi123456789@users.noreply.github.com>
2020-02-09 21:56:14 +0530

    index update

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const queue =  require('@data_structure_js/dsa/src/queue/queue');
 const circularQueue =  require('@data_structure_js/dsa/src/circularQueue/circularQueue');
 const linkedList =  require('@data_structure_js/dsa/src/linkedList/linkedList');
 const doublyLinkedList =  require('@data_structure_js/dsa/src/doublyLinkedList/doublyLinkedList');
-module.exports = {stack,queue,circularQueue,linkedList,doublyLinkedList};
+const circularSinglyLinkedList =  require('@data_structure_js/dsa/src/circularSinglyLinkedList/circularSinglyLinkedList');
+module.exports = {stack,queue,circularQueue,linkedList,doublyLinkedList,circularSinglyLinkedList};
